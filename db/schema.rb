bbc16e79722dcd4e1cfc9de5727edace6ae8f623
Gregory Kallai <gerg.kallai@gmail.com>
2019-02-26 11:37:38 +0100

    Adds associations between data models
    Co-authored-by: Aditya Naik <naik.aditya@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_26_102443) do
+ActiveRecord::Schema.define(version: 2019_02_26_102822) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,10 @@ ActiveRecord::Schema.define(version: 2019_02_26_102443) do
     t.string "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.bigint "chat_id"
+    t.index ["chat_id"], name: "index_messages_on_chat_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -43,4 +47,6 @@ ActiveRecord::Schema.define(version: 2019_02_26_102443) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "messages", "chats"
+  add_foreign_key "messages", "users"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Message.destroy_all
+Chat.destroy_all
+User.destroy_all
+
+user1 = User.create(email: 'user1@mail.com', password:'password')
+user2 = User.create(email: 'user2@mail.com', password:'password')
+
+chat = Chat.create()
+chat.users << user1
+chat.users << user2
+
+5.times do
+  Message.create(text:'Hej', user_id: user1.id, chat_id: chat.id)
+end
