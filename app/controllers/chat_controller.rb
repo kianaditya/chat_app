391e41d9c0c87e092fa98449969d51110c190916
Gregory Kallai <gerg.kallai@gmail.com>
2019-02-27 12:01:17 +0100

    Refactors chat controller action to follow the single responsibility dogma
    Co-authored-by: Aditya Naik <naik.aditya@gmail.com>

--- app/controllers/chat_controller.rb
+++ app/controllers/chat_controller.rb
@@ -1,42 +1,52 @@
 class ChatController < ApplicationController
-    def index
-      if user_signed_in?
-        @chats = current_user.chats
-        @users = get_available_chats
+  def index
+    if user_signed_in?
+      @chats = current_user.chats
+      @users = get_available_chats
+    end
+  end
+
+  def show
+    @chat = Chat.find(params[:id])
+    @messages = Message.where(chat_id: params[:id])
+  end
+
+  def create
+    user = User.find_by_id(params[:user])
+    chat = Chat.new()
+    if chat.save
+      chat.users << current_user
+      chat.users << user
+      redirect_to chat_path(chat)
+    end
+  end
+
+  private
+
+  def get_available_chats
+    existing_chat_users = get_existing_chat_users
+    users = get_users(existing_chat_users)
+    users
+  end
+
+  def get_existing_chat_users
+    existing_chat_users = []
+    @chats.each do |chat|
+      chat.users.each do |user|
+        existing_chat_users.push(user)
       end
     end
-
-    def show
-      @chat = Chat.find(params[:id])
-      @messages = Message.where(chat_id: params[:id])
-    end
-
-    def create
-      user = User.find_by_id(params[:user])
-      chat = Chat.new()
-      if chat.save
-        chat.users << current_user
-        chat.users << user
-        redirect_to chat_path(chat)
+    existing_chat_users
+  end
+  
+  def get_users(existing_chat_users)
+    users = []
+    all_users = User.all
+    all_users.each do |user|
+      unless existing_chat_users.include?(user)
+        users.push(user)
       end
     end
-
-    private
-
-    def get_available_chats
-      existing_chat_users = []
-      users = []
-      all_users = User.all
-      @chats.each do |chat|
-        chat.users.each do |user|
-          existing_chat_users.push(user)
-        end
-      end
-      all_users.each do |user|
-        unless existing_chat_users.include?(user)
-          users.push(user)
-        end
-      end
-      users
-    end
+    users
+  end
 end
